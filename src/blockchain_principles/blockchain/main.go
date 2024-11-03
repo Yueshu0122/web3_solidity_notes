357d50e367f8599dbf24bd283d7f31b092577ad0
YutongNi <2731989n@student.gla.ac.uk>
2024-11-03 22:32:51 +0800

    blockchain with transaction

--- src/blockchain_principles/blockchain/main.go
+++ src/blockchain_principles/blockchain/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"sync"
 	"time"
 
 	"github.com/joho/godotenv"
 )
+
+var Blockchain []Block
+var Mempool []Transaction
+var mutex = &sync.Mutex{}
 
 func main() {
 	err := godotenv.Load()
@@ -15,24 +22,52 @@ func main() {
 		log.Fatal(err)
 	}
 
-	go func() {
-		t := time.Now()
-		genesisBlock := &Block{}
-		genesisBlock = NewBlock(0, t, 0, "")
-		mutex.Lock()
-		Blockchain = append(Blockchain, *genesisBlock)
-		mutex.Unlock()
-	}()
+	// 通过命令行参数指定端口号
+	portPtr := flag.Int("port", 8080, "HTTP server port")
+	flag.Parse()
+	httpPort := os.Getenv("PORT")
+	if httpPort == "" {
+		httpPort = strconv.Itoa(*portPtr)
+	}
 
-	go func() {
-		nodes := []string{"http://node1.example.com", "http://node2.example.com"}
-		for {
-			syncBlockchain(nodes)
-			time.Sleep(10 * time.Second) // 每10秒同步一次
-		}
-	}()
+	go initializeGenesisBlock()
+	go syncBlockchainPeriodically()
+	go generateBlocksPeriodically()
 
 	log.Fatal(run())
+}
+
+func initializeGenesisBlock() {
+	t := time.Now()
+	genesisBlock := NewBlock(0, t, 0, "", []Transaction{})
+	mutex.Lock()
+	Blockchain = append(Blockchain, *genesisBlock)
+	mutex.Unlock()
+}
+
+func syncBlockchainPeriodically() {
+	nodes := nodeList
+	for {
+		syncBlockchain(nodes)
+		time.Sleep(10 * time.Second) // 每10秒同步一次
+	}
+}
+
+func generateBlocksPeriodically() {
+	for {
+		time.Sleep(30 * time.Second) // 每30秒生成一个新区块
+		mutex.Lock()
+		if len(Mempool) > 0 {
+			prevBlock := Blockchain[len(Blockchain)-1]
+			newBlock := generateBlock(prevBlock, 0, Mempool)
+			if isBlockValid(newBlock, prevBlock) {
+				Blockchain = append(Blockchain, newBlock)
+				Mempool = []Transaction{} // 清空内存池
+				broadcastNewBlock(newBlock)
+			}
+		}
+		mutex.Unlock()
+	}
 }
 
 func run() error {
--- src/blockchain_principles/blockchain/utils.go
+++ src/blockchain_principles/blockchain/utils.go
@@ -1,21 +1,57 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
+	"net/http"
 	"time"
 )
 
-func generateBlock(oldBlock Block, BPM int) Block {
+func generateBlock(oldBlock Block, BPM int, transactions []Transaction) Block {
 	t := time.Now()
 
 	newBlock := Block{
-		Index:     oldBlock.Index + 1,
-		Timestamp: t.String(),
-		BPM:       BPM,
-		PrevHash:  oldBlock.Hash,
+		Index:        oldBlock.Index + 1,
+		Timestamp:    t.String(),
+		BPM:          BPM,
+		PrevHash:     oldBlock.Hash,
+		Transactions: transactions,
 	}
 	nonce, hash := proofOfWork(newBlock)
 	newBlock.Nonce = nonce
 	newBlock.Hash = hash
 
 	return newBlock
+}
+
+func syncBlockchain(nodes []string) {
+	longestChain := Blockchain
+	longestLength := len(Blockchain)
+
+	for _, node := range nodes {
+		resp, err := http.Get(node + "/")
+		if err != nil {
+			log.Printf("Failed to fetch blockchain from %s: %v\n", node, err)
+			continue
+		}
+		defer resp.Body.Close()
+
+		var remoteBlockchain []Block
+		if err := json.NewDecoder(resp.Body).Decode(&remoteBlockchain); err != nil {
+			log.Printf("Failed to decode blockchain from %s: %v\n", node, err)
+			continue
+		}
+
+		if len(remoteBlockchain) > longestLength && isChainValid(remoteBlockchain) {
+			longestChain = remoteBlockchain
+			longestLength = len(remoteBlockchain)
+		}
+	}
+
+	if !slicesEqual(longestChain, Blockchain) {
+		mutex.Lock()
+		Blockchain = longestChain
+		mutex.Unlock()
+		log.Println("Blockchain updated to the longest chain")
+	}
 }
